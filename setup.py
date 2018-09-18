2f9c6027f134aa9e4142145ba8d1dcd82bbf3590
lbusoni <lbusoni@gmail.com>
2018-09-18 17:58:31 +0200

    new setup with version, twine upload and git tag

--- setup.py
+++ setup.py
@@ -1,10 +1,67 @@
 #!/usr/bin/env python
-from setuptools import setup
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import setup, Command
+
+NAME = 'pysilico'
+DESCRIPTION = 'Allied Vision AVT-Prosilica camera controller with PLICO'
+URL = 'https://github.com/lbusoni/pysilico'
+EMAIL = 'lorenzo.busoni@inaf.it'
+AUTHOR = 'Lorenzo Busoni'
+LICENSE= 'MIT'
+KEYWORDS= 'plico, prosilica, avt, camera, laboratory, instrumentation control',
 
 
-setup(name='pysilico',
-      description='AVT-Prosilica camera controller with PLICO',
-      version='0.12',
+here = os.path.abspath(os.path.dirname(__file__))
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), about)
+
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+setup(name=NAME,
+      description=DESCRIPTION,
+      version=about['__version__'],
       classifiers=['Development Status :: 4 - Beta',
                    'Operating System :: POSIX :: Linux',
                    'Programming Language :: Python :: 2.7',
@@ -12,11 +69,11 @@ setup(name='pysilico',
                    'Programming Language :: Python :: 3.6',
                    ],
       long_description=open('README.md').read(),
-      url='',
-      author_email='lbusoni@gmail.com',
-      author='Lorenzo Busoni',
-      license='',
-      keywords='plico, prosilica, avt, camera, laboratory, instrumentation control',
+      url=URL,
+      author_email=EMAIL,
+      author=AUTHOR,
+      license=LICENSE,
+      keywords=KEYWORDS,
       packages=['pysilico',
                 'pysilico.calibration',
                 'pysilico.client',
@@ -32,7 +89,7 @@ setup(name='pysilico',
       package_data={
           'pysilico': ['conf/pysilico.conf'],
       },
-      install_requires=["plico>=0.14",
+      install_requires=["plico>=0.15",
                         "numpy",
                         "psutil",
                         "configparser",
@@ -48,4 +105,5 @@ setup(name='pysilico',
                         ],
       include_package_data=True,
       test_suite='test',
+      cmdclass={'upload': UploadCommand, },
       )
