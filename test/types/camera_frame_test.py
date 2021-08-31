ba7cc454472a57feeace6f202b0269d2a7bfc95f
Lorenzo Busoni <lbusoni@gmail.com>
2021-08-31 16:21:11 +0200

    fix warning, remove dependencies, bump  version

--- test/types/camera_frame_test.py
+++ test/types/camera_frame_test.py
@@ -4,18 +4,14 @@ import numpy as np
 from pysilico.types.camera_frame import CameraFrame
 
 
-__version__ = "$Id: camera_frame_test.py 163 2016-12-03 21:41:27Z lbusoni $"
-
-
 class CameraFrameTest(unittest.TestCase):
 
     def setUp(self):
         self._createCameraFrame()
 
-
     def _createCameraFrame(self):
         self._frame = np.arange(
-            120* 100, dtype=np.uint8).reshape((120, 100))
+            120 * 100, dtype=np.uint8).reshape((120, 100))
         self._counter = 42
         self._cameraFrame = CameraFrame.fromNumpyArray(self._frame,
                                                        self._counter)
@@ -25,16 +21,13 @@ class CameraFrameTest(unittest.TestCase):
         frame2 = self._cameraFrame.toNumpyArray()
         self.assertTrue(np.array_equal(self._frame, frame2))
 
-
     def testTypeIsFloat(self):
-        self.assertEqual(np.float, self._cameraFrame.toNumpyArray().dtype)
-
+        self.assertEqual(float, self._cameraFrame.toNumpyArray().dtype)
 
     def testComparison(self):
         counter = 42
         cameraFrame2 = CameraFrame.fromNumpyArray(self._frame.copy(), counter)
         self.assertTrue(self._cameraFrame == cameraFrame2)
-
 
     def testStringRepresentation(self):
         self.assertEqual('frame-42', str(self._cameraFrame))
